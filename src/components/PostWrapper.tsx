1bfa3440837c571d6cd2752322b51a353b12dde4
Resi Respati <resir014@gmail.com>
2018-02-15 00:24:29 +0700

    added header image to PostWrapper

--- src/components/PostWrapper.tsx
+++ src/components/PostWrapper.tsx
@@ -10,17 +10,27 @@ interface PostWrapperProps {
 }
 
 const ArticleHeader = styled.header`
-  padding: ${dimensions.containerPadding};
-  padding-bottom: 0;
+  position: relative;
+  width: 100%;
+`
+
+const ArticleHeaderImage = styled.img`
+  width: 100%;
+  max-height: 20rem;
+  object-fit: cover;
+  vertical-align: middle;
 `
 
 const ArticleTitle = styled.h1`
   margin: 0;
+  padding: ${dimensions.containerPadding};
+  padding-bottom: .5rem;
   line-height: ${dimensions.lineHeight.heading};
 `
 
 const ArticleContent = styled(MarkdownContent)`
   padding: ${dimensions.containerPadding};
+  padding-top: 0;
 
   p:first-of-type {
     margin-top: 0;
@@ -36,11 +46,29 @@ const ArticleContent = styled(MarkdownContent)`
   }
 `
 
+const CardMetadata = styled.section`
+  display: flex;
+  flex-direction: column;
+  padding: 0 ${dimensions.containerPadding};
+  padding-bottom: 1.5rem;
+  font-size: 85%;
+  color: ${colors.gray.calm};
+`
+
+const CardMetadataAuthor = styled.p`
+  margin: 0;
+`
+
 const PostWrapper: React.SFC<PostWrapperProps> = ({ article }) => (
   <PageCard>
     <ArticleHeader>
+      <ArticleHeaderImage src={article.thumbnail} alt={article.title} />
       <ArticleTitle>{article.title}</ArticleTitle>
     </ArticleHeader>
+    <CardMetadata>
+      <CardMetadataAuthor>{article.author}</CardMetadataAuthor>
+      <CardMetadataAuthor>{article.pubDate}</CardMetadataAuthor>
+    </CardMetadata>
     <ArticleContent html={article.content} />
   </PageCard>
 )
